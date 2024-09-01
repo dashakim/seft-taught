28ec4a96e27c1ff12ca9cec4dc2a0fcb9b4348e5
Daria Tabala <dasha.tabala@gmail.com>
2024-09-01 10:52:36 +0300

    Add sequences

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Define a list of names
+names = ['Harry', 'Ron','Hermione']
+
+# add to the array
+names.append('Draco')
+
+print(names)
+
+names.sort()
+
+print(names)
---
+++ sequences.py
@@ -0,0 +1,13 @@
+name = "Harry"
+
+print(name[3])
+
+# tuple
+
+coordinateX = 10.3
+
+coordinateY = 2
+
+coordinate = (10.3, 2)
+
+print(coordinate)
