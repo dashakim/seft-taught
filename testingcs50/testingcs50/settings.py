ee8817927cd8bbcd397b72c45563fa87437f8c00
Daria Tabala <dasha.tabala@gmail.com>
2024-11-15 16:40:31 +0000

    Small fix

--- testingcs50/testingcs50/settings.py
+++ testingcs50/testingcs50/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=u@_i@ep=4z6q7ot2ing28qxc4oy4bkl@&o9axj=c2aly1ssq'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
